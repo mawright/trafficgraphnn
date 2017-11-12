4c6a2c235cc85e59cc9bede01f79396fb98c5f33
mawright <mwright@berkeley.edu>
2017-11-11 17:45:53 -0800

    Add reverse neighborhoods to minibatching in directed graphs

--- trafficgraphnn/minibatch.py
+++ trafficgraphnn/minibatch.py
@@ -1,8 +1,11 @@
+from __future__ import print_function, absolute_import, division
+
 import math
 from lxml import etree
 import pandas as pd
 import numpy as np
 import networkx as nx
+import six
 
 from keras.utils import Sequence
 
@@ -28,6 +31,11 @@ class NodeMinibatcher(Sequence):
         self.features = features
         self.targets = targets
         self.include_back_hops = include_back_hops
+        if graph.is_directed and include_back_hops:
+            self.neigh_range = range(-filter_path_length,
+                                     filter_path_length + 1)
+        else:
+            self.neigh_range = range(filter_path_length + 1)
 
         self.data = self._build_data_df()
 
@@ -56,10 +64,10 @@ class NodeMinibatcher(Sequence):
                                 int(round(float(interval.attrib['begin']))),
                                 node,
                                 det_id,
+                                # )] = dict(interval.items())
                                 )] = {col: interval.attrib[col]
                                       for col in columns_to_get
                                       if col in interval.keys()}
-                                # )] = dict(interval.items())
 
         df = pd.DataFrame.from_dict(records, orient='index', dtype=float)
         df.index.set_names(['time', 'node', 'det_id'], inplace=True)
@@ -74,39 +82,48 @@ class NodeMinibatcher(Sequence):
             index * self.batch_size:(index + 1) * self.batch_size]
 
         if self.graph.is_directed:
-            neighs = [self._directed_neighs(self.graph, batch_indeces)]
-            if self.include_back_hops:
-                neighs.append(self._reverse_neighs(self.graph, batch_indeces))
+            neighs = self._directed_neighs(self.graph, batch_indeces)
+            if self.include_back_hops:  # add negative-hop neighborhoods
+                reverse_neighs = self._reverse_neighs(
+                    self.graph, batch_indeces, True)
+                for n, steps in six.iteritems(neighs):
+                    rn = reverse_neighs[n]
+                    for reverse_neighbor, r_steps in six.iteritems(rn):
+                        if (
+                            reverse_neighbor not in steps
+                            or abs(steps[reverse_neighbor]) > abs(r_steps)
+                        ):
+                            steps[reverse_neighbor] = r_steps
         else:
-            neighs = [self._undirected_neighs(self.graph, batch_indeces)]
+            neighs = self._undirected_neighs(self.graph, batch_indeces)
 
         time_sliced = self.data.iloc[
             self.data.index.get_level_values('time').isin(
                 [i[0] for i in batch_indeces])
         ]
 
+        # done to remove nans if two detector types in one node
         meaned_over_detector = time_sliced.groupby(
             level=['time', 'node'], sort=False).mean()
 
         batch_x = []
         concatted_y = np.zeros((len(batch_indeces), len(self.targets)))
-        for index in batch_indeces:
+        for i, index in enumerate(batch_indeces):
             time = index[0]
             neigh = neighs[index[1]]
-            neigh_sizes = [neigh.values().count(k)
-                           for k in range(self.filter_path_length + 1)]
+            neigh_sizes = [neigh.values().count(k) for k in self.neigh_range]
             node_x = np.zeros((
-                self.filter_path_length + 1,
+                len(neigh_sizes),
                 max(neigh_sizes),
                 len(self.features)))
 
             index_for_neigh = np.zeros(node_x.shape[0], dtype=int)
-            for node, k in neigh.items():
+            for node, k in six.iteritems(neigh):
                 node_x[k, index_for_neigh[k]] = meaned_over_detector.loc[
                     (time, node), self.features]
                 index_for_neigh[k] += 1
 
-            concatted_y[index] = meaned_over_detector.loc[
+            concatted_y[i] = meaned_over_detector.loc[
                 (time, node), self.targets]
 
             batch_x.append(node_x)
@@ -131,10 +148,14 @@ class NodeMinibatcher(Sequence):
         }
         return directed_neigh
 
-    def _reverse_neighs(self, graph, indeces):
+    def _reverse_neighs(self, graph, indeces, return_negative=True):
         reverse_graph = graph.reverse()
-        reverse_neigh = self._directed_neighs(reverse_graph, indeces)
-        return reverse_neigh
+        reverse_neighs = self._directed_neighs(reverse_graph, indeces)
+        if return_negative:
+            for neigh in six.itervalues(reverse_neighs):
+                for k, v in six.iteritems(neigh):
+                    neigh[k] = -v
+        return reverse_neighs
 
     def _undirected_neighs(self, graph, indeces):
         undirected_neigh = {
