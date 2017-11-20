bfa6a218ee9d650012b82153fd3d483456a08f81
mawright <mwright@berkeley.edu>
2017-11-20 12:38:18 -0800

    add validation batcher to queue learner

--- trafficgraphnn/models.py
+++ trafficgraphnn/models.py
@@ -12,16 +12,16 @@ class SupervisedQueueLearner(object):
         use_undirected_graph=False, num_filters=32,
         filter_path_length=2, include_back_hops=False,
         fc_layer_sizes=[128, 64], batch_size=64,
+        validation_network_file=None, validation_addl_files=None,
         validation_output_files=None
     ):
         if lanewise:
-            self.graph = sumo_network.get_lane_graph(
-                network_file, undirected=use_undirected_graph,
-                detector_files=addl_files)
+            graph_getter = sumo_network.get_lane_graph
         else:
-            self.graph = sumo_network.get_edge_graph(
-                network_file, undirected=use_undirected_graph,
-                detector_files=addl_files)
+            graph_getter = sumo_network.get_edge_graph
+        self.graph = graph_getter(
+            network_file, undirected=use_undirected_graph,
+            detector_files=addl_files)
 
         self.batcher = NodeMinibatcher(
             self.graph, filter_path_length, data_files=output_files,
@@ -29,8 +29,11 @@ class SupervisedQueueLearner(object):
             features=features)
 
         if validation_output_files is not None:
+            val_graph = graph_getter(
+                validation_network_file, undirected=use_undirected_graph,
+                detector_files=validation_addl_files)
             self.validation_batcher = NodeMinibatcher(
-                self.graph, filter_path_length, validation_output_files,
+                val_graph, filter_path_length, validation_output_files,
                 batch_size=batch_size, include_back_hops=include_back_hops,
                 features=features)
         else:
