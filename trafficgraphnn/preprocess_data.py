c03679444785e67c2064734753068e2dd2c09625
rc_ehlers <simon.ehlers@stud.uni-hannover.de>
2018-07-16 15:58:50 -0700

    implemented and tested method "get_liu_results" and "get_phase_for_time" in preprocess_data.py

--- trafficgraphnn/preprocess_data.py
+++ trafficgraphnn/preprocess_data.py
@@ -20,14 +20,47 @@ from trafficgraphnn.utils import iterfy
 _logger = logging.getLogger(__name__)
 
 class PreprocessData(object):
-    def __init__(self, sumo_net):
-        self.sumo_net = sumo_net
+    def __init__(self, sumo_network):
+        self.sumo_network = sumo_network
+        self.graph = self.sumo_network.get_graph()
         self.liu_results_path = os.path.join(os.path.dirname(
-                self.sumo_net.netfile), 'liu_estimation_results.h5')
+                self.sumo_network.netfile), 'liu_estimation_results.h5')
+        self.df_liu_results = pd.DataFrame()
+        try:
+            self.df_liu_results = pd.read_hdf(self.liu_results_path)
+        except IOError:
+            print('An error occured trying to read the hdf file.')
+                
+
         
-    def get_liu_results(self, phase, lane_ID):
-        #this code should return a the results of the liu_method 
-        #for a specific lane and phase    
+    def get_liu_results(self, lane_ID, phase):
+        series_results = self.df_liu_results.loc[phase, lane_ID]        
+        # return series with parameter time, ground-truth,
+        # estimation hybrid/ pure liu
+        return series_results
         
-        
+    def get_phase_for_time(self, lane_ID, point_of_time):
+        #this code should give the phase for a specific point of time and lane back
+        time_lane = self.df_liu_results.loc[:, (lane_ID, 'time')] 
+               
+        for phase in range(1, len(time_lane)):
+            if (phase > 2 and 
+                point_of_time <= time_lane[phase] and 
+                point_of_time > time_lane[phase-1]
+                ):                
+                return phase
+            
+            if (point_of_time <= time_lane[1]):
+                return 1
+            
+    
+    def preprocess_detector_data(self, average_interval, average_over_cycle = False):
+        #code that is averaging over specific intervals 
+        #(eg. 5s, 15s, 30s; given in a list) and an option to average 
+        #over a whole cycle. For each averaging interval a new h5 file is created
         pass
+    
+    def unload_data(self):
+        #this code should unload all the data and give memory free
+        pass
+    
