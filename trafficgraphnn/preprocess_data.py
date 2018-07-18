51cf6f7b00142fb004b7230bffd77ec19f62f333
rc_ehlers <simon.ehlers@stud.uni-hannover.de>
2018-07-17 18:46:03 -0700

    Implemented preprocessing of e1 detector data in file 'preprocess_data.py'

--- trafficgraphnn/preprocess_data.py
+++ trafficgraphnn/preprocess_data.py
@@ -25,6 +25,8 @@ class PreprocessData(object):
         self.graph = self.sumo_network.get_graph()
         self.liu_results_path = os.path.join(os.path.dirname(
                 self.sumo_network.netfile), 'liu_estimation_results.h5')
+        self.detector_data_path =  os.path.join(os.path.dirname(
+                self.sumo_network.netfile), 'output')
         self.df_liu_results = pd.DataFrame()
         try:
             self.df_liu_results = pd.read_hdf(self.liu_results_path)
@@ -54,12 +56,106 @@ class PreprocessData(object):
                 return 1
             
     
-    def preprocess_detector_data(self, average_interval, average_over_cycle = False):
+    def preprocess_detector_data(self, average_intervals, average_over_cycle = False):
         #code that is averaging over specific intervals 
         #(eg. 5s, 15s, 30s; given in a list) and an option to average 
-        #over a whole cycle. For each averaging interval a new h5 file is created
-        pass
-    
+        #over a whole cycle. For each averaging interval a new .h5 file is created
+        
+        file_list = os.listdir(self.detector_data_path)
+        str_e1 = 'e1'
+       
+        for interval in average_intervals:
+            print('processing interval ', interval)
+            self.df_interval_results = pd.DataFrame() #reset df for every interval
+            
+            for filename in file_list:
+                if str_e1 in filename:                    
+                    df_detector = self.process_e1_data(filename, interval)
+                    self.df_interval_results = pd.concat(
+                            [self.df_interval_results, df_detector], axis = 1)                   
+                  
+            self.df_interval_results.to_hdf(os.path.join(os.path.dirname(self.sumo_network.netfile),
+                            'e1_detector_data_'+ str(interval) + '_seconds.h5'),
+                            key = 'df_estimation_results')
+                
+    def process_e1_data(self, filename, interval):
+        append_cnt=0    #counts how often new data were appended
+        list_nVehContrib = []
+        list_flow = []
+        list_occupancy = []
+        list_speed = []
+        list_length = []
+        
+        memory_nVehContrib = []
+        memory_flow = []
+        memory_occupancy = []
+        memory_speed = []
+        memory_length = []
+        memory_end_time = []
+        
+        df_detector = pd.DataFrame()
+        
+        for event, elem in et.iterparse(os.path.join(self.detector_data_path, filename)):
+            
+            if elem.tag == 'interval':
+            
+                list_nVehContrib.append(float(elem.attrib.get('nVehContrib')))
+                list_flow.append(float(elem.attrib.get('flow')))
+                list_occupancy.append(float(elem.attrib.get('occupancy')))
+                list_speed.append(float(elem.attrib.get('speed')))
+                list_length.append(float(elem.attrib.get('length')))            
+                append_cnt += 1
+                
+                if append_cnt == interval:
+                    
+                    append_cnt = 0
+                    #process data
+                    memory_nVehContrib.append(sum(list_nVehContrib))
+                    memory_flow.append(sum(list_flow)/interval)
+                    memory_occupancy.append(sum(list_occupancy)/interval)
+                    
+                    cnt_veh = 0
+                    sum_speed = 0
+                    sum_length = 0
+                    for speed, length in zip(list_speed, list_length):
+                        if speed != -1: #when speed != -1 is also length != -1
+                            sum_speed = sum_speed + speed
+                            sum_length = sum_length + length
+                            cnt_veh += 1
+                    if cnt_veh > 0:
+                        memory_speed.append(sum_speed/cnt_veh)
+                        memory_length.append(sum_length/cnt_veh)
+                    else:
+                        memory_speed.append(-1)
+                        memory_length.append(-1)                        
+                        
+                    #append data to dataframe          
+                    memory_end_time.append(int(float(elem.attrib.get('end'))))
+                    detector_id = elem.attrib.get('id')
+                    
+                    #reset temporary lists
+                    list_nVehContrib = []
+                    list_flow = []
+                    list_occupancy = []
+                    list_speed = []
+                    list_length = []
+                                
+        iterables = [[detector_id], [
+                'nVehContrib', 'flow', 'occupancy', 'speed', 'length']]
+        index = pd.MultiIndex.from_product(iterables, names=['detector ID', 'values'])
+        df_detector = pd.DataFrame(index = [memory_end_time], columns = index)
+        df_detector.index.name = 'end time'
+        
+        # fill interval_series with values
+        df_detector[detector_id, 'nVehContrib'] = memory_nVehContrib
+        df_detector[detector_id, 'flow'] = memory_flow
+        df_detector[detector_id, 'occupancy'] = memory_occupancy
+        df_detector[detector_id, 'speed'] = memory_speed
+        df_detector[detector_id, 'length'] = memory_length
+       
+        return df_detector 
+
+
     def unload_data(self):
         #this code should unload all the data and give memory free
         pass
