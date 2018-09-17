bugfix for average_interval != 1

gat_and_attn_seq2seq.py
@@ -50,8 +50,8 @@ show_plot = False
 show_infos = False
 
 ### Configuration for preprocessing the detector data
-average_interval = 1  #Attention -> right now no other average interval than 1 is possible  -> bugfix necessary!
-sample_size = 10
+average_interval = 10  #Attention -> right now no other average interval than 1 is possible  -> bugfix necessary!
+sample_size = 15     #number of steps per sample in size of average interval
 
 ### Configuration of the deep learning model
 width_1gat = 100 # Output dimension of first GraphAttention layer
@@ -60,8 +60,8 @@ n_attn_heads = 5              # Number of attention heads in first GAT layer
 dropout_rate = 0            # Dropout rate applied to the input of GAT layers
 attn_dropout = 0            #Dropout of the adjacency matrix in the gat layer
 l2_reg = 5e-100               # Regularization rate for l2
-learning_rate = 5e-3       # Learning rate for SGD
-epochs = 3             # Number of epochs to run for
+learning_rate = 0.001      # Learning rate for optimizer
+epochs = 2            # Number of epochs to run for
 es_patience = 100             # Patience fot early stopping
 n_units = 128   #number of units of the LSTM cells
 
@@ -79,7 +79,7 @@ config.gen_e1_detectors(distance_to_tls=[5, 125], frequency=1)
 config.gen_e2_detectors(distance_to_tls=0, frequency=1)
 config.define_tls_output_file()
 
-for train_num in range(1):
+for train_num in range(2):
 
     # run the simulation to create output files
     list_X = []
@@ -107,7 +107,7 @@ for train_num in range(1):
         
         ### preprocess data for deep learning model
         preprocess = PreprocessData(sn, simu_num)
-        preproccess_end_time = preprocess.get_preprocessing_end_time(liu_runner.get_liu_lane_IDs())
+        preproccess_end_time = preprocess.get_preprocessing_end_time(liu_runner.get_liu_lane_IDs(), average_interval)
         A, X, Y, order_lanes = preprocess.preprocess_A_X_Y(
                 average_interval = average_interval, sample_size = sample_size, start = 200, end = preproccess_end_time, simu_num = simu_num)
         
@@ -214,7 +214,7 @@ for train_num in range(1):
         decoder_output = AttentionDecoder(n_units, 1)(decoder_inputs)
         model = Model(inputs=X1_in, outputs=decoder_output)
 
-        optimizer = Adagrad(lr=learning_rate)
+        optimizer = Adam(lr=learning_rate)
         model.compile(optimizer=optimizer,
                       loss='mean_squared_error',
                       weighted_metrics=['accuracy'])
@@ -272,7 +272,7 @@ liu_runner.plot_results_every_lane(show_plot = show_plot, show_infos = show_info
 
 ### preprocess data for deep learning model
 preprocess = PreprocessData(sn, simu_num)
-preproccess_end_time = preprocess.get_preprocessing_end_time(liu_runner.get_liu_lane_IDs())
+preproccess_end_time = preprocess.get_preprocessing_end_time(liu_runner.get_liu_lane_IDs(), average_interval)
 A, X_test_tens, Y_test_tens, order_lanes_test = preprocess.preprocess_A_X_Y(
         average_interval = average_interval, sample_size = sample_size, start = 200, end = preproccess_end_time, simu_num = simu_num)
 A = np.eye(N,N) + A
@@ -313,4 +313,4 @@ print("Saved attn model to disk")
 
 df_liu_results_test = pd.read_hdf(liu_runner.get_liu_results_path())
 
-store_predictions_in_df(prediction, order_lanes_test, df_liu_results_test, 200, preproccess_end_time)
+store_predictions_in_df(prediction, order_lanes_test, df_liu_results_test, 200, preproccess_end_time, average_interval)

trafficgraphnn/postprocess_predictions.py
@@ -24,13 +24,13 @@ def resample_predictions(predictions):
         for sample in range(num_samples):
             if sample == 0:
                 lane_series = lane_slice[sample, :]
-                lane_series = np.reshape(lane_series, (1,10))
+                lane_series = np.reshape(lane_series, (1,timesteps_per_sample))
                 #print('sample = 0!!; lane_series.shape:', lane_series.shape)
             else:
                 #print('lane_series.shape in first else:', lane_series.shape)
                 #print('sample != 0; lane_series_reshaped.shape:', np.reshape(lane_series, (1, lane_series.shape[0])).shape)
                 #print('lane_slice[sample, :].shape:', np.reshape(lane_slice[sample, :],(1, 10)).shape)
-                lane_series = np.hstack((lane_series, np.reshape(lane_slice[sample, :], (1,10))))
+                lane_series = np.hstack((lane_series, np.reshape(lane_slice[sample, :], (1,timesteps_per_sample))))
                 #print('lane_series.shape after hstack:',lane_series.shape)
         #print('lane_series.shape before reshape:', lane_series.shape)
         lane_series = np.reshape(lane_series, (1, num_samples*timesteps_per_sample))
@@ -40,7 +40,7 @@ def resample_predictions(predictions):
     
     return predictions_resampled
 
-def store_predictions_in_df(predictions, order_lanes, df_liu_results, start_time, end_time):
+def store_predictions_in_df(predictions, order_lanes, df_liu_results, start_time, end_time, average_interval):
     resampled_predictions = resample_predictions(predictions)
     resampled_predictions = np.transpose(resampled_predictions)
     #print('resampled_predictions.shape (transpose):', resampled_predictions.shape)
@@ -48,7 +48,7 @@ def store_predictions_in_df(predictions, order_lanes, df_liu_results, start_time
     #list_columns = df_liu_results.columns.unique(level = 'lane')
     #print('list_coumns:', list_columns)
     df_prediction_results = pd.DataFrame(data = resampled_predictions[:,:], 
-                                         index = range(start_time, resampled_predictions.shape[0]+start_time), 
+                                         index = range(start_time, resampled_predictions.shape[0] * average_interval + start_time, average_interval), 
                                          columns = order_lanes)
     df_prediction_results.to_hdf('nn_prediction_results.h5', key = 'prediciton_results')
     print('Stored prediction results in dataframe')

trafficgraphnn/preprocess_data.py
@@ -262,22 +262,24 @@ class PreprocessData(object):
             
             if cnt_lane == 0:
                 #dimesion: time x nodes x features
-                duration_in_sec = len(df_detector[stopbar_detector_id]['nVehContrib'][start_time:end_time])
-                X_unsampled = np.zeros((int((duration_in_sec)/average_interval), len(subgraph.nodes), 9))
-                Y_unsampled = np.zeros((int((duration_in_sec)/average_interval), len(subgraph.nodes), 1))
+                num_rows = len(df_detector.loc[start_time:end_time, (stopbar_detector_id, 'nVehContrib')]) -1 # (-1):last row is belongs to the following average interval
+                duration_in_sec = end_time-start_time
+                X_unsampled = np.zeros((num_rows, len(subgraph.nodes), 9))
+                Y_unsampled = np.zeros((num_rows, len(subgraph.nodes), 1))
                 
-            X_unsampled[:, cnt_lane, 0] = df_detector[stopbar_detector_id]['nVehContrib'][start_time:end_time]
-            X_unsampled[:, cnt_lane, 1] = df_detector[stopbar_detector_id]['flow'][start_time:end_time]
-            X_unsampled[:, cnt_lane, 2] = df_detector[stopbar_detector_id]['occupancy'][start_time:end_time]
-            X_unsampled[:, cnt_lane, 3] = df_detector[stopbar_detector_id]['speed'][start_time:end_time]
-            X_unsampled[:, cnt_lane, 4] = df_detector[adv_detector_id]['nVehContrib'][start_time:end_time]
-            X_unsampled[:, cnt_lane, 5] = df_detector[adv_detector_id]['flow'][start_time:end_time]
-            X_unsampled[:, cnt_lane, 6] = df_detector[adv_detector_id]['occupancy'][start_time:end_time]
-            X_unsampled[:, cnt_lane, 7] = df_detector[adv_detector_id]['speed'][start_time:end_time]
+            X_unsampled[:, cnt_lane, 0] = df_detector.loc[start_time:end_time-average_interval, (stopbar_detector_id, 'nVehContrib')]
+            X_unsampled[:, cnt_lane, 1] = df_detector.loc[start_time:end_time-average_interval, (stopbar_detector_id, 'flow')]
+            X_unsampled[:, cnt_lane, 2] = df_detector.loc[start_time:end_time-average_interval, (stopbar_detector_id, 'occupancy')]
+            X_unsampled[:, cnt_lane, 3] = df_detector.loc[start_time:end_time-average_interval, (stopbar_detector_id, 'speed')]
+            X_unsampled[:, cnt_lane, 4] = df_detector.loc[start_time:end_time-average_interval, (adv_detector_id, 'nVehContrib')]
+            X_unsampled[:, cnt_lane, 5] = df_detector.loc[start_time:end_time-average_interval, (adv_detector_id, 'flow')]
+            X_unsampled[:, cnt_lane, 6] = df_detector.loc[start_time:end_time-average_interval, (adv_detector_id, 'occupancy')]
+            X_unsampled[:, cnt_lane, 7] = df_detector.loc[start_time:end_time-average_interval, (adv_detector_id, 'speed')]
             X_unsampled[:, cnt_lane, 8] = self.get_tls_binary_signal(start_time, duration_in_sec, lane, average_interval)
                   
             Y_unsampled[:, cnt_lane, 0] = self.get_ground_truth_array(start_time, duration_in_sec, lane, average_interval)
-
+            
+            
         num_samples = math.ceil(X_unsampled.shape[0]/sample_size)
         print('number of samples:', num_samples)
         X_sampled = np.zeros((num_samples, sample_size, len(subgraph.nodes), 9))
@@ -365,9 +367,11 @@ class PreprocessData(object):
         self.parsed_xml_tls = None
         pass
     
-    def get_preprocessing_end_time(self, liu_lanes):
+    def get_preprocessing_end_time(self, liu_lanes, average_interval):
         list_end_time = []
         for lane in liu_lanes:
             df_last_row = self.df_liu_results.iloc[-1, :]  
             list_end_time.append(df_last_row.loc[(lane,'phase end')])
-        return min(list_end_time)-10 #10seconds reserve, otherwise complications occur
+            end_time = min(list_end_time)-10 #10seconds reserve, otherwise complications occur
+            end_time = int(end_time/average_interval) * average_interval #make sure, that end time is matching with average_interval
+            return end_time