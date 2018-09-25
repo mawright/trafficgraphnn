3c5e896d1db47f9e3fd5e82449eea9b04b2f2be3
Matthew A. Wright <mwright@berkeley.edu>
2018-09-25 10:55:50 -0700

    add time_distributed_multi_input.py

---
+++ trafficgraphnn/time_distributed_multi_input.py
@@ -0,0 +1,147 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from six.moves import zip
+from keras.engine.base_layer import Layer
+from keras.engine.base_layer import InputSpec
+from keras.utils.generic_utils import has_arg
+from keras.utils.generic_utils import object_list_uid
+from keras.utils.generic_utils import to_list
+from keras import backend as K
+
+from keras.layers import recurrent
+from keras.layers.wrappers import Wrapper, TimeDistributed
+
+
+class TimeDistributedMultiInput(TimeDistributed):
+    def __init__(self, layer, **kwargs):
+        super(TimeDistributedMultiInput, self).__init__(layer, **kwargs)
+
+    def build(self, input_shape):
+        # If a singleton shape tuple is passed in, use the vanilla TimeDistributed
+        if not isinstance(input_shape, list):
+            super(TimeDistributedMultiInput, self).build(input_shape)
+            return
+
+        assert all([len(shape) >= 3 for shape in input_shape if shape is not None])
+        # We need to verify that the inputs have the same or broadcastable batch
+        # and time dimensions
+        batch_sizes = [shape[0] for shape in input_shape if shape is not None]
+        batch_sizes = set(batch_sizes)
+        batch_sizes -= set([None])
+        # TODO?: Allow batch size of 1 if an input is to be broadcasted
+        # batch_sizes -= set([1])
+        # self._broadcast_batch = [shape[0] == 1 if shape is not None else False
+        #                          for shape in input_shape]
+
+        if len(batch_sizes) > 1:
+            raise ValueError('Receieved tensors with incompatible batch sizes. '
+                            'Got tensors with shapes : ' + str(input_shape))
+        timesteps = [shape[1] for shape in input_shape if shape is not None]
+        timesteps = set(timesteps)
+        timesteps -= set(None)
+        # TODO? Allow 1 timestep if an input is to be broadcasted
+        # timesteps -= set([1])
+        # self._broadcast_time = [shape[1] == 1 if shape is not None else False
+        #                          for shape in input_shape]
+
+        if len(timesteps) > 1:
+            raise ValueError('Receieved tensors with incompatible number of timesteps. '
+                            'Got tensors with shapes : ' + str(input_shape))
+        self.timesteps = timesteps.pop()
+        self.input_spec = [InputSpec(shape=K.int_shape(shape))
+                        for shape in input_shape]
+        child_input_shapes = [(shape[0],) + shape[2:] for shape in input_shape]
+        if not self.layer.built:
+            self.layer.build(child_input_shapes)
+            self.layer.built = True
+
+        Wrapper.build(self)
+
+    def compute_output_shape(self, input_shape):
+        if not isinstance(input_shape, list):
+            return super(
+                TimeDistributedMultiInput, self).compute_output_shape(
+                    input_shape)
+
+        child_input_shapes = [(shape[0],) + shape[2:] for shape in input_shape]
+        child_output_shape = self.layer.compute_output_shape(child_input_shapes)
+        if hasattr(self, 'timesteps'):
+            timesteps = self.timesteps
+        else:
+            timesteps = input_shape[0][1]
+        return (child_output_shape[0], timesteps) + child_output_shape[1:]
+
+    def call(self, inputs, training=None, mask=None):
+        if not isinstance(inputs, list):
+            return super(TimeDistributedMultiInput, self).call(inputs,
+                                                               training=training,
+                                                               mask=mask)
+
+        kwargs = {}
+        if has_arg(self.layer.call, 'training'):
+            kwargs['training'] = training
+        uses_learning_phase = False
+
+        input_shapes = [K.int_shape(input) for input in inputs]
+        batch_sizes = [shape[0] for shape in input_shapes if shape is not None]
+        fixed_batch_size = any([bs is not None for bs in batch_sizes])
+        if fixed_batch_size:
+            # batch size matters, use rnn-based implementation
+            def step(x, _):
+                global uses_learning_phase
+                output = self.layer.call(x, **kwargs)
+                if hasattr(output, '_uses_learning_phase'):
+                    uses_learning_phase = (output._uses_learning_phase or
+                                           uses_learning_phase)
+                return output, []
+
+            # Note: will likely fail here if K.rnn doesn't like multiple inputs
+            _, outputs, _ = K.rnn(step, inputs,
+                                  initial_states=[],
+                                  input_length=input_shapes[1],
+                                  unroll=False)
+            y = outputs
+        else:
+            # No batch size specified, therefore the layer will be able
+            # to process batches of any size.
+            # We can go with reshape-based implementation for performance.
+
+            input_length = self.timesteps if self.timesteps else K.shape(inputs[0])[1]
+            # ^^ assumes input 0 has the correct number of timesteps
+            for inp in inputs:
+                inner_input_shape = self._get_shape_tuple((-1,), inp, 2)
+                # Shape: (num_samples * timesteps, ...). And track the
+                # transformation in self._input_map.
+                input_uid = object_list_uid(inp)
+                inp = K.reshape(inp, inner_input_shape)
+                self._input_map[input_uid] = inp
+            # (num_samples * timesteps, ...)
+            if has_arg(self.layer.call, 'mask') and mask is not None:
+                inner_mask_shape = self._get_shape_tuple((-1,), mask, 2)
+                kwargs['mask'] = K.reshape(mask, inner_mask_shape)
+            y = self.layer.call(inputs, **kwargs)
+
+            if isinstance(y, list):
+                raise NotImplementedError(
+                    'TimeDistributedMultiInput not implemented for multiple '
+                    'output tensors yet.')
+
+            if hasattr(y, '_uses_learning_phase'):
+                uses_learning_phase = y._uses_learning_phase
+            # Shape: (num_samples, timesteps, ...)
+            output_shape = self.compute_output_shape(input_shapes)
+            output_shape = self._get_shape_tuple(
+                (-1, input_length), y, 1, output_shape[2:])
+            y = K.reshape(y, output_shape)
+
+        # Apply activity regularizer if any:
+        if (hasattr(self.layer, 'activity_regularizer') and
+           self.layer.activity_regularizer is not None):
+            regularization_loss = self.layer.activity_regularizer(y)
+            self.add_loss(regularization_loss, inputs)
+
+        if uses_learning_phase:
+            y._uses_learning_phase = True
+        return y
