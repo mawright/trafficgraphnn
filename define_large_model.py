47732866cc9efa58f5814c1fd02745a8ff5b29d1
Matthew A. Wright <mwright@berkeley.edu>
2018-10-05 17:57:27 -0700

    update reshape layers

--- define_large_model.py
+++ define_large_model.py
@@ -35,13 +35,13 @@ l2_reg = 5e-5               # Regularization rate for l2
 learning_rate = 0.001      # Learning rate for optimizer
 n_units = 128   #number of units of the LSTM cells
 
-def define_model(num_simulations, num_timesteps, num_lanes, num_features, A): 
-          
+def define_model(num_simulations, num_timesteps, num_lanes, num_features, A):
+
         X1_in = Input(batch_shape=(None, num_timesteps, num_lanes, num_features))
         A_in = Input(batch_shape=(None, num_timesteps, num_lanes, num_lanes))
-        
+
         dropout1 = TimeDistributed(Dropout(dropout_rate))(X1_in)
-        
+
         graph_attention_1 = TimeDistributedMultiInput(BatchGraphAttention(width_1gat,
                                            attn_heads=n_attn_heads,
                                            attn_heads_reduction='average',
@@ -50,9 +50,9 @@ def define_model(num_simulations, num_timesteps, num_lanes, num_features, A):
                                            kernel_initializer='random_uniform',
                                            kernel_regularizer=l2(l2_reg))
                                            )([dropout1, A_in])
-        
+
         dropout2 = TimeDistributed(Dropout(dropout_rate))(graph_attention_1)
-        
+
         graph_attention_2 = TimeDistributedMultiInput(BatchGraphAttention(F_,
                                            attn_heads=n_attn_heads,
                                            attn_heads_reduction='average',
@@ -60,9 +60,9 @@ def define_model(num_simulations, num_timesteps, num_lanes, num_features, A):
                                            activation='linear',
                                            kernel_regularizer=l2(l2_reg),
                                            kernel_initializer='random_uniform'))([dropout2, A_in])
-        
+
         dropout3 = TimeDistributed(Dropout(dropout_rate))(graph_attention_2)
-        
+
         graph_attention_3 = TimeDistributedMultiInput(BatchGraphAttention(F_,
                                            attn_heads=n_attn_heads,
                                            attn_heads_reduction='average',
@@ -70,39 +70,39 @@ def define_model(num_simulations, num_timesteps, num_lanes, num_features, A):
                                            activation='linear',
                                            kernel_regularizer=l2(l2_reg),
                                            kernel_initializer='random_uniform'))([dropout3, A_in])
-        
+
         dropout4 = TimeDistributed(Dropout(dropout_rate))(graph_attention_3)
-        
-        dense1 = TimeDistributed(Dense(128, activation = linear, 
-                                       kernel_regularizer=l2(l2_reg), 
+
+        dense1 = TimeDistributed(Dense(128, activation = linear,
+                                       kernel_regularizer=l2(l2_reg),
                                        kernel_initializer='random_uniform'))(dropout4)
-        
+
         dropout5 = TimeDistributed(Dropout(dropout_rate))(dense1)
-        
-        dense2 = TimeDistributed(Dense(128, activation = linear, 
-                               kernel_regularizer=l2(l2_reg), 
+
+        dense2 = TimeDistributed(Dense(128, activation = linear,
+                               kernel_regularizer=l2(l2_reg),
                                kernel_initializer='random_uniform'))(dropout5)
-        
+
         dropout6 = TimeDistributed(Dropout(dropout_rate))(dense2)
-        
+
         #make sure that the reshape is made correctly!
-        encoder_inputs = ReshapeForLSTM(num_simulations)(dropout6)
-  
+        encoder_inputs = ReshapeForLSTM()(dropout6)
+
         decoder_inputs = LSTM(n_units,
-                              return_sequences=True, 
+                              return_sequences=True,
                               kernel_initializer='random_uniform')(encoder_inputs)
 
         decoder_output = AttentionDecoder(n_units, 2, causal=True)(decoder_inputs) #Attention! 2 output features now!
 
         reshaped_output = ReshapeForOutput(num_lanes)(decoder_output)
 
-        model = Model(inputs= [X1_in, A_in], outputs=reshaped_output) 
-        
+        model = Model(inputs= [X1_in, A_in], outputs=reshaped_output)
+
         optimizer = Adam(lr=learning_rate)
         model.compile(optimizer=optimizer,
                       loss='mean_squared_error',
                       metrics=['mean_absolute_percentage_error'])
         model.summary()
         plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-        
+
         return model
--- trafficgraphnn/reshape_layers.py
+++ trafficgraphnn/reshape_layers.py
@@ -10,88 +10,54 @@ from keras import backend as K
 from keras.engine.topology import Layer
 
 class ReshapeForLSTM(Layer):
-
     def __init__(self,
-                 num_simulations,
                  **kwargs):
-        self.num_simulations = num_simulations
         super(ReshapeForLSTM, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        self.input_dim = input_shape
-#        self.num_simulations = self.input_dim[0]
-        self.num_timesteps = self.input_dim[1]
-        self.num_lanes = self.input_dim[2]
-        self.num_features = self.input_dim[3]
-        super(ReshapeForLSTM, self).build(input_shape)  # Be sure to call this at the end
 
     def call(self, x):
         x_permuted = K.permute_dimensions(x, (0, 2, 1, 3))
-#        x_reshaped = K.reshape(x_permuted, (self.num_simulations*self.num_lanes, self.num_timesteps, self.num_features))
-        x_reshaped = K.reshape(x_permuted, (-1, self.num_timesteps, self.num_features))
+        shape = K.shape(x_permuted)
+        x_reshaped = K.reshape(x_permuted, (-1, shape[2], shape[3]))
         return x_reshaped
 
     def compute_output_shape(self, input_shape):
-        return (self.num_simulations*self.num_lanes, self.num_timesteps, self.num_features)
-    
-    def get_config(self):
-        """
-            For rebuilding models on load time.
-        """
-        config = {
-#            'input_dim': self.input_dim,
-            'num_simulations': self.num_simulations,
-#            'num_timesteps': self.num_timesteps,
-#            'num_lanes': self.num_lanes,
-#            'num_features': self.num_features
-        }
-        base_config = super(ReshapeForLSTM, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
+        if input_shape[0] is not None:
+            first_dim = input_shape[0] * input_shape[2]
+        else:
+            first_dim = None
+        return (first_dim, input_shape[1], input_shape[3])
 
-#    @classmethod
-#    def from_config(cls, config):
-#        return cls(**config)
-    
+    def get_config(self):
+        base_config = super(ReshapeForLSTM, self).get_config()
+        return base_config
+
+
 class ReshapeForOutput(Layer):
 
-    def __init__(self, 
+    def __init__(self,
                  num_lanes,
                  **kwargs):
         self.num_lanes = num_lanes
         super(ReshapeForOutput, self).__init__(**kwargs)
 
-    def build(self, input_shape):
-        self.input_dim = input_shape
-#        self.num_simulations_x_num_lanes = self.input_dim[0]
-        self.num_timesteps = self.input_dim[1]
-        self.num_features = self.input_dim[2]
-        super(ReshapeForOutput, self).build(input_shape)  # Be sure to call this at the end
-
     def call(self, x):
-        x_reshaped = K.reshape(x, (-1, self.num_lanes, self.num_timesteps, self.num_features))
+        shape = K.shape(x)
+        assert shape.shape == 3 # number of dimensions
+        x_reshaped = K.reshape(x, (-1, self.num_lanes, shape[-2], shape[-1]))
         x_permuted = K.permute_dimensions(x_reshaped, (0, 2, 1, 3))
         self.shape_output = x_permuted.shape
         return x_permuted
 
     def compute_output_shape(self, input_shape):
-        #return (self.num_simulations_x_num_lanes//self.num_lanes, self.num_timesteps, self.num_lanes, self.num_features)
-        return self.shape_output
-    
+        shape = [-1, input_shape[-2], self.num_lanes, input_shape[-1]]
+        return tuple(shape)
+
     def get_config(self):
         """
             For rebuilding models on load time.
         """
         config = {
-#            'input_dim': self.input_dim,
-#            'num_simulations_x_num_lanes': self.num_simulations_x_num_lanes,
-#            'num_timesteps': self.num_timesteps,
             'num_lanes': self.num_lanes
-#            'num_features': self.num_features,
-#            'shape_output': self.shape_output
         }
         base_config = super(ReshapeForOutput, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-    
-#    @classmethod
-#    def from_config(cls, config):
-#        return cls(**config)
