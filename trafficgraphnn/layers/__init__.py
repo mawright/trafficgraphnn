f31bb0162f835dbd4d4133a72bdbd98fc9674147
Matthew A. Wright <mwright@berkeley.edu>
2018-11-15 09:35:37 -0800

    refactor the reshaping layers

--- trafficgraphnn/layers/__init__.py
+++ trafficgraphnn/layers/__init__.py
@@ -1,5 +1,6 @@
 from .attention_decoder import AttentionDecoder
-from .time_distributed_multi_input import TimeDistributedMultiInput
 from .batch_graph_attention_layer import BatchGraphAttention
-from .reshape_layers import ReshapeForLSTM, ReshapeForOutput
 from .batch_shaw_multigraph_attention_layer import BatchShawMultigraphAttention
+from .reshape_layers import (ReshapeFoldInLanes, ReshapeForLSTM,
+                             ReshapeForOutput, ReshapeUnfoldLanes)
+from .time_distributed_multi_input import TimeDistributedMultiInput
--- trafficgraphnn/layers/reshape_layers.py
+++ trafficgraphnn/layers/reshape_layers.py
@@ -9,15 +9,27 @@ Created on Wed Sep 26 10:47:56 2018
 from keras import backend as K
 from keras.engine.topology import Layer
 
-class ReshapeForLSTM(Layer):
+
+class ReshapeFoldInLanes(Layer):
     def __init__(self,
                  **kwargs):
-        super(ReshapeForLSTM, self).__init__(**kwargs)
+        super(ReshapeFoldInLanes, self).__init__(**kwargs)
 
     def call(self, x):
         x_permuted = K.permute_dimensions(x, (0, 2, 1, 3))
         shape = K.shape(x_permuted)
-        x_reshaped = K.reshape(x_permuted, (-1, shape[2], shape[3]))
+        int_shape = K.int_shape(x_permuted)
+
+        if int_shape[2] is not None:
+            shape2 = int_shape[2]
+        else:
+            shape2 = shape[2]
+        if int_shape[3] is not None:
+            shape3 = int_shape[3]
+        else:
+            shape3 = shape[3]
+
+        x_reshaped = K.reshape(x_permuted, (-1, shape2, shape3))
         return x_reshaped
 
     def compute_output_shape(self, input_shape):
@@ -28,22 +40,32 @@ class ReshapeForLSTM(Layer):
         return (first_dim, input_shape[1], input_shape[3])
 
     def get_config(self):
-        base_config = super(ReshapeForLSTM, self).get_config()
+        base_config = super(ReshapeFoldInLanes, self).get_config()
         return base_config
 
 
-class ReshapeForOutput(Layer):
-
+class ReshapeUnfoldLanes(Layer):
     def __init__(self,
                  num_lanes,
                  **kwargs):
         self.num_lanes = num_lanes
-        super(ReshapeForOutput, self).__init__(**kwargs)
+        super(ReshapeUnfoldLanes, self).__init__(**kwargs)
 
     def call(self, x):
         shape = K.shape(x)
         assert shape.shape == 3 # number of dimensions
-        x_reshaped = K.reshape(x, (-1, self.num_lanes, shape[-2], shape[-1]))
+        int_shape = K.int_shape(x)
+
+        if int_shape[1] is not None:
+            shape1 = int_shape[1]
+        else:
+            shape1 = shape[1]
+        if int_shape[2] is not None:
+            shape2 = int_shape[2]
+        else:
+            shape2 = shape[2]
+
+        x_reshaped = K.reshape(x, (-1, self.num_lanes, shape1, shape2))
         x_permuted = K.permute_dimensions(x_reshaped, (0, 2, 1, 3))
         self.shape_output = x_permuted.shape
         return x_permuted
@@ -60,5 +82,10 @@ class ReshapeForOutput(Layer):
         config = {
             'num_lanes': self.num_lanes
         }
-        base_config = super(ReshapeForOutput, self).get_config()
+        base_config = super(ReshapeUnfoldLanes, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+
+# legacy names
+ReshapeForLSTM = ReshapeFoldInLanes
+ReshapeForOutput = ReshapeUnfoldLanes
