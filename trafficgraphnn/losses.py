62a8d344c524b824cc219a45b82ba63a145254d8
Matthew A. Wright <mwright@berkeley.edu>
2018-11-26 22:32:40 -0800

    update losses

--- trafficgraphnn/losses.py
+++ trafficgraphnn/losses.py
@@ -1,5 +1,5 @@
 from keras import backend as K
-from keras.losses import mean_absolute_error
+from keras.losses import mean_absolute_error, mean_squared_error
 
 def nan_masked_mse(y_true, y_pred):
     import tensorflow as tf
@@ -15,8 +15,22 @@ def negative_masked_mse(y_true, y_pred):
 def _masked_mse(y_true, y_pred, to_mask):
     mask_weight = 1 - K.cast(to_mask, K.floatx())
     sq_error = K.square(y_pred - y_true)
-    masked_error = sq_error * mask_weight
-    return K.sum(masked_error) / K.sum(mask_weight)
+    return _apply_mask(sq_error, mask_weight)
+
+def mean_square_error_veh(y_true, y_pred):
+    return mean_squared_error(y_true[...,0], y_pred[...,0])
+
+
+def negative_masked_mse_queue_length_m(y_true, y_pred):
+    return negative_masked_mse(y_true[...,1], y_pred[...,1])
+
+
+def scaled_two_feature_mse_constructor(scaling_weight=10):
+    def scaled_two_feature_mse(y_true, y_pred):
+        loss_1 = mean_square_error_veh(y_true, y_pred)
+        loss_2 = negative_masked_mse_queue_length_m(y_true, y_pred)
+        return scaling_weight * loss_1 + loss_2
+    return scaled_two_feature_mse
 
 
 def negative_masked_mae(y_true, y_pred):
@@ -35,8 +49,12 @@ def negative_masked_mae_queue_length_m(y_true, y_pred):
 def _masked_mae(y_true, y_pred, to_mask):
     mask_weight = 1 - K.cast(to_mask, K.floatx())
     abs_error = K.abs(y_pred - y_true)
-    masked_error = abs_error * mask_weight
-    # sum_error = K.sum(masked_error, axis=-1)
-    # sum_weight = K.sum(mask_weight, axis=-1)
-    # return K.switch(sum_weight == 0., 0, sum_error / sum_weight)
-    return K.sum(masked_error) / K.sum(mask_weight)
+    return _apply_mask(abs_error, mask_weight)
+    # return K.sum(masked_error) / K.sum(mask_weight)
+
+
+def _apply_mask(error, mask_weights):
+    masked_error = error * mask_weights
+    sum_error = K.sum(masked_error)
+    sum_weight = K.sum(mask_weights)
+    return K.switch(K.equal(sum_weight, 0.), 0., sum_error / sum_weight)
