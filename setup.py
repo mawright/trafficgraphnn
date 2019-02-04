de9611c8b0c492a83740eff00ab67a1c5432bed1
Matthew A. Wright <mwright@berkeley.edu>
2019-02-03 21:13:26 -0800

    update requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='trafficgraphnn',
     version='0.1',
     packages=['trafficgraphnn'],
-    install_requires=['keras'],
+    install_requires=requirements,
     url='',
     license='',
     author='Matthew A. Wright',
