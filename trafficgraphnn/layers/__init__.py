e1c8980d845a4f14bdf1255d971420301b4490d5
Matthew A. Wright <mwright@berkeley.edu>
2019-02-08 11:02:56 -0800

    multigraph attention layer

--- trafficgraphnn/layers/__init__.py
+++ trafficgraphnn/layers/__init__.py
@@ -1,4 +1,5 @@
 from .batch_graph_attention_layer import BatchGraphAttention
+from .batch_multigraph_attention_layer import BatchMultigraphAttention
 from .reshape_layers import (ReshapeFoldInLanes, ReshapeForLSTM,
                              ReshapeForOutput, ReshapeUnfoldLanes)
 from .time_distributed_multi_input import TimeDistributedMultiInput
--- trafficgraphnn/layers/batch_multigraph_attention_layer.py
+++ trafficgraphnn/layers/batch_multigraph_attention_layer.py
@@ -87,7 +87,7 @@ class BatchMultigraphAttention(Layer):
 
         if self.use_bias:
             self.biases = [
-                self.add_weight(shape=(self.F_,), # same bias for every node
+                self.add_weight(shape=(self.F_ * self.num_edge_types,), # same bias for every node
                                 initializer=self.bias_initializer,
                                 regularizer=self.bias_regularizer,
                                 constraint=self.bias_constraint,
@@ -117,9 +117,10 @@ class BatchMultigraphAttention(Layer):
         self.built = True
 
     def call(self, inputs):
-        raise NotImplementedError
         X = inputs[0]  # Node features (batch x N x F)
-        A = inputs[1]  # Adjacency matrices (E-long list of (N x N))
+        A = inputs[1]  # Adjacency matrices (E-long list of (batch x N x N))
+
+        A_stacked = K.stack(A, -1) # (batch x N x N x E)
 
         outputs = []
         for h in range(self.attn_heads):
@@ -130,57 +131,88 @@ class BatchMultigraphAttention(Layer):
                 bias = self.biases[h]
 
             # Compute inputs to attention network
-            features = K.dot(X, kernel)  # (batch N x F')
+            features = K.dot(X, kernel)  # (batch x N x F')
 
             # Compute feature combinations
-            # Note: [[a_1], [a_2]]^T [[Wh_i], [Wh_2]] = [a_1]^T [Wh_i] + [a_2]^T [Wh_j]
+            # Note: [[a_i], [a_j]]^T [[Wh_i], [Wh_2]] = [a_i]^T [Wh_i] + [a_j]^T [Wh_j]
             attn_for_self = K.dot(features, attn_kernel_self)    # (N x 1), [a_1]^T [Wh_i]
-            attn_for_neighs = K.dot(features, attn_kernel_neighs)  # (N x 1), [a_2]^T [Wh_j]
+            # (N x 1), [a_j^c]^T [Wh_j^c]
+            attn_for_neighs = [K.dot(features, k) for k in attn_kernel_neighs]
 
             # Attention head a(Wh_i, Wh_j) = a^T [[Wh_i], [Wh_j]]
 
             #transpose just dimension 1 and 2, not batches
-            trans_attn_for_neighs = K.permute_dimensions(attn_for_neighs, perm=[0, 2, 1])
-            scores = attn_for_self + trans_attn_for_neighs  # (N x N) via broadcasting
+            trans_attn_for_neighs = [K.permute_dimensions(an, [0, 2, 1])
+                                     for an in attn_for_neighs] # (batch x 1 x N)
+            stacked_attn_for_neighs = K.stack(trans_attn_for_neighs, 1) # (batch x E x 1 x N)
+            attn_for_self = K.expand_dims(attn_for_self, 1) # (batch x 1 x N x 1)
+            scores = attn_for_self + stacked_attn_for_neighs  # (batch x E x N x N) via broadcasting
 
             # Add nonlinearty
             scores = LeakyReLU(alpha=0.2)(scores)
 
             # Mask values before activation (Vaswani et al., 2017)
-            mask = K.exp(A * -10e9) * -10e9
+            mask = (1.0 - A_stacked) * -10e9 # (batch x E x N x N)
             masked = scores + mask
 
             # Feed masked values to softmax
-            softmax = K.softmax(masked)  # (N x N), attention coefficients
-            dropout = Dropout(self.attn_dropout)(softmax)  # (N x N)
+            softmax = K.softmax(masked)  # (batch x E x N x N), attention coefficients
+            dropout = Dropout(self.attn_dropout)(softmax)  # (batch x E x N x N)
+
+            slices = [dropout[:,i] for i in range(self.num_edge_types)]
 
             # Linear combination with neighbors' features
-            node_features = K.batch_dot(dropout, features)  # (N x F')
+            node_features = [K.batch_dot(x, features) for x in slices] # E x (batch x N x F')
+
+            node_features = K.concatenate(node_features, -1) # (batch x N x EF')
 
             if self.use_bias:
                 node_features = K.bias_add(node_features, bias)
-
-            if self.attn_heads_reduction == 'concat' and self.activation is not None:
-                # In case of 'concat', we compute the activation here (Eq 5)
-                node_features = self.activation(node_features)
 
             # Add output of attention head to final output
             outputs.append(node_features)
 
         # Reduce the attention heads output according to the reduction method
         if self.attn_heads_reduction == 'concat':
-            output = K.concatenate(outputs)  # (N x KF')
+            output = K.concatenate(outputs, -1)  # (batch x N x EKF')
         else:
-            output = K.mean(K.stack(outputs), axis=0)  # N x F')
-            if self.activation is not None:
-                # In case of 'average', we compute the activation here (Eq 6)
-                output = self.activation(output)
+            output = K.mean(K.stack(outputs, axis=0), axis=0)  #( batch x N x EF')
+
+        output = self.activation(output)
 
         return output
 
     def compute_output_shape(self, input_shape):
         X_shape = input_shape[0]
+        num_edge_types = len(input_shape[-1])
         assert input_shape[-1] is not None
-        output_shape = list(input_shape)
-        output_shape[-1] = self.output_dim
+        output_shape = list(X_shape)
+        output_shape[-1] = self.output_dim * num_edge_types
         return tuple(output_shape)
+
+    def get_config(self):
+        """
+            For rebuilding models on load time.
+        """
+        config = {
+            'F_': self.F_,
+            'attn_heads': self.attn_heads,
+            'num_edge_types': self.num_edge_types,
+            'attn_heads_reduction': self.attn_heads_reduction,
+            'attn_dropout': self.attn_dropout,
+            'feature_dropout': self.feature_dropout,
+            'use_bias': self.use_bias,
+            'activation': self.activation,
+            'kernel_initializer': self.kernel_initializer,
+            'bias_initializer': self.bias_initializer,
+            'attn_kernel_initializer': self.attn_kernel_initializer,
+            'kernel_regularizer': self.kernel_regularizer,
+            'bias_regularizer': self.bias_regularizer,
+            'attn_kernel_regularizer': self.attn_kernel_regularizer,
+            'activity_regularizer': self.activity_regularizer,
+            'kernel_constraint': self.kernel_constraint,
+            'bias_constraint': self.bias_constraint,
+            'attn_kernel_constraint': self.attn_kernel_constraint
+        }
+        base_config = super(BatchMultigraphAttention, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
