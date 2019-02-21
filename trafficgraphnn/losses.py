d56ec8ccd410beb7514068353e34845912595e97
Matthew A. Wright <mwright@berkeley.edu>
2019-02-21 15:33:34 -0800

    update losses

--- trafficgraphnn/losses.py
+++ trafficgraphnn/losses.py
@@ -12,10 +12,16 @@ def negative_masked_mse(y_true, y_pred):
     return _masked_mse(y_true, y_pred, is_neg)
 
 
+def negative_masked_mape(y_true, y_pred):
+    is_neg = K.less(y_true, 0.)
+    return _masked_mape(y_true, y_pred, is_neg)
+
+
 def _masked_mse(y_true, y_pred, to_mask):
     mask_weight = 1 - K.cast(to_mask, K.floatx())
     sq_error = K.square(y_pred - y_true)
     return _apply_mask(sq_error, mask_weight)
+
 
 def mean_square_error_veh(y_true, y_pred):
     return mean_squared_error(y_true[...,0], y_pred[...,0])
@@ -50,7 +56,14 @@ def _masked_mae(y_true, y_pred, to_mask):
     mask_weight = 1 - K.cast(to_mask, K.floatx())
     abs_error = K.abs(y_pred - y_true)
     return _apply_mask(abs_error, mask_weight)
-    # return K.sum(masked_error) / K.sum(mask_weight)
+
+
+def _masked_mape(y_true, y_pred, to_mask):
+    mask_weight = 1 - K.cast(to_mask, K.floatx())
+    num = K.abs(y_pred - y_true)
+    den = K.clip(K.abs(y_true), K.epsilon(), None)
+    percent_error = num / den
+    return 100. * _apply_mask(percent_error, mask_weight)
 
 
 def _apply_mask(error, mask_weights):
