d2acdc3af89d79fb9bee81046bc8eeb66396b71a
Matthew A. Wright <mwright@berkeley.edu>
2019-02-28 22:55:37 -0800

    heuristic for fixing undercounting of input-output queue due to lane changes

--- trafficgraphnn/preprocessing/liumethod_new.py
+++ trafficgraphnn/preprocessing/liumethod_new.py
@@ -14,8 +14,15 @@ from trafficgraphnn.utils import DetInfo
 JAM_DENSITY = 0.13333 # hardcoded default jam density value (veh/meter)
 
 
+# codes for estimation methods
+__INPUT_OUTPUT = 'input-output'
+__SATURATED = 'saturated'
+__LIU = 'liu'
+
+
 def liu_method_for_net(sumo_network, output_data_hdf_filename,
-                       jam_density=JAM_DENSITY, num_workers=None):
+                       jam_density=JAM_DENSITY, num_workers=None,
+                       use_lane_change_accounting_heuristic=True):
     args = []
     lane_ids = []
     idds = []
@@ -30,9 +37,99 @@ def liu_method_for_net(sumo_network, output_data_hdf_filename,
     with multiprocessing.Pool(num_workers) as pool:
         results = pool.starmap(liu_for_lane, args)
 
-    out = {lane: result for lane, result in zip(lane_ids, results)}
+    # out = {lane: result for lane, result in zip(lane_ids, results)}
 
-    return out
+    out = {result.columns[0]: result for result in results}
+
+    return results
+    # return out
+
+
+def postprocess_negative_from_lane_changes(sn, results):
+    """Heuristic to try and mitigate negative net loop flows from lane changes.
+
+    Lane queue estimates computed by the simple input output method will
+    miss lane-changing vehicles. This function identifies the lanes that
+    have an apparent negative queue from input-output accounting and
+    decreases the road's other lanes queues by that amount as a heuristic.
+    """
+    grouped_lanes = _split_lanes_by_edges(sn, results)
+    dfs = {}
+    for lane_group in grouped_lanes.values():
+        lane_ids = [df.columns[0] for df in lane_group]
+        df = pd.concat(lane_group, axis=1, join='outer', keys=lane_ids)
+        df.columns = pd.MultiIndex.from_product([lane_ids,
+                                                 ['estimate', 'method']])
+
+        is_io_estimate = df.loc[:, (slice(None), 'method')] == __INPUT_OUTPUT
+        io_estimates = (df.loc[:, (slice(None), 'estimate')]
+                        * is_io_estimate.values)
+
+        deficit_assigned = distribute_deficit_loop(io_estimates)
+        df.update(deficit_assigned)
+        df.loc[:, (slice(None), 'estimate')
+               ] = df.loc[:, (slice(None), 'estimate')].clip(lower=0)
+
+        dfs.update((lane, lane_df.dropna())
+                   for lane, lane_df in df.groupby(axis=1, level=0))
+
+    return dfs
+
+
+def distribute_io_estimate_deficit_for_edge(lane_estimate_dfs_for_edge):
+    lane_ids = [df.columns[0] for df in lane_estimate_dfs_for_edge]
+    df = pd.concat(lane_estimate_dfs_for_edge,
+                   axis=1, join='outer', keys=lane_ids)
+    df.columns = pd.MultiIndex.from_product([lane_ids,
+                                                ['estimate', 'method']])
+
+    is_io_estimate = df.loc[:, (slice(None), 'method')] == __INPUT_OUTPUT
+    io_estimates = (df.loc[:, (slice(None), 'estimate')]
+                    * is_io_estimate.values)
+
+    deficit_assigned = distribute_deficit_loop(io_estimates)
+    df.update(deficit_assigned)
+    df.loc[:, (slice(None), 'estimate')
+            ] = df.loc[:, (slice(None), 'estimate')].clip(lower=0)
+
+    return df
+
+
+def distribute_deficit_loop(df):
+    while df[df < 0].count().sum() > 0:
+        df = distribute_deficit_one_iter(df)
+    return df
+
+
+def distribute_deficit_one_iter(df):
+    negatives = df[df < 0]
+    positives = df[df > 0]
+    sum_negs = negatives.sum(axis=1)
+    num_pos = positives.count(axis=1)
+
+    to_add = sum_negs / num_pos
+    added = positives.add(to_add, axis=0)
+
+    return added
+
+
+def _split_lanes_by_edges(sn, results):
+    lane_groups = {}
+    lanes = list(results.keys())
+    while len(lanes) > 0:
+        lane = lanes[0]
+        edge = sn.net.getLane(lane).getEdge()
+        all_lanes_in_edge = [ln.getID() for ln in edge.getLanes()]
+        lane_group = []
+        for ln in all_lanes_in_edge:
+            try:
+                lane_group.append(results.pop(ln))
+            except KeyError:
+                continue
+            lanes.remove(ln)
+        lane_groups[edge.getID()] = lane_group
+
+    return lane_groups
 
 
 def raw_output_data_to_hdf(sumo_network):
@@ -120,9 +217,15 @@ def liu_for_lane(output_data_hdf_filename, lane_id, inter_detector_distance,
 
         residual_queue_estimate = max(estimates_list[-1][1], 0)
 
-    # return tuples of the time that a liu estimate is made and the estimate
-    # itself
-    return estimates_list
+    # return pandas df of the time that a liu estimate is made
+    # and the estimate itself
+    result = pd.DataFrame(
+        estimates_list, columns=['begin',
+                                 '{}'.format(lane_id),
+                                 '{}_method'.format(lane_id)])
+    result = result.set_index('begin')
+    return result
+    # return estimates_list
 
 
 def breakpoints_A_B(advance_df):
@@ -218,22 +321,22 @@ def queue_estimate(stopbar_df, advance_df, green_start,
                    prev_phase_queue_estimate, inter_detector_distance,
                    jam_density):
     if breakpoint_A is None:
-        return input_output_method(stopbar_df, advance_df,
-                                   prev_phase_queue_estimate, green_start)
+        time, estimate = input_output_method(stopbar_df, advance_df,
+                                             prev_phase_queue_estimate,
+                                             green_start)
+        return time, estimate, __INPUT_OUTPUT
     elif breakpoint_C is None:
         # No breakpoint C means that the queue is saturated and therefore
         # unobservable. In this case the Liu method gives no guidance, so we
         # choose to estimate that the queue reaches the advance detector
         phase_end = stopbar_df.index[-1]
         saturated_queue_estimate_veh = inter_detector_distance * jam_density
-        return phase_end, saturated_queue_estimate_veh
+        return phase_end, saturated_queue_estimate_veh, __SATURATED
     else:
-        return liu_estimate_from_breakpoints(advance_df, stopbar_df,
-                                             green_start,
-                                             breakpoint_B, breakpoint_C,
-                                             inter_detector_distance,
-                                             jam_density)
-
+        (t_max, q_max), (t_end, q_end) = liu_estimate_from_breakpoints(
+            advance_df, stopbar_df, green_start, breakpoint_B, breakpoint_C,
+            inter_detector_distance, jam_density)
+        return (t_max, q_max, __LIU), (t_end, q_end, __LIU)
 
 def input_output_method(stopbar_df, advance_df, prev_phase_queue_estimate,
                         green_time):
@@ -244,7 +347,7 @@ def input_output_method(stopbar_df, advance_df, prev_phase_queue_estimate,
     queue_estimate = prev_phase_queue_estimate + net_flow
     assert stopbar_df.index[-1] == advance_df.index[-1]
     t_queue_estimate = stopbar_df.index[-1]
-    return t_queue_estimate, max(queue_estimate, 0)
+    return t_queue_estimate, queue_estimate
 
 
 def liu_estimate_from_breakpoints(advance_df, stopbar_df, green_start,
@@ -254,8 +357,12 @@ def liu_estimate_from_breakpoints(advance_df, stopbar_df, green_start,
     """Liu's "Expansion I" method (Equation 11)"""
 
     # estimate shockwave speed
-    v2 = inter_detector_distance / (breakpoint_B - green_start)
-    v2 = abs(v2)
+
+    if breakpoint_B != green_start:
+        v2 = inter_detector_distance / (breakpoint_B - green_start)
+        v2 = abs(v2)
+    else:
+        v2 = np.inf
 
     # max queue
     n = advance_df.loc[:breakpoint_C, 'nVehContrib'].sum()
@@ -273,10 +380,8 @@ def liu_estimate_from_breakpoints(advance_df, stopbar_df, green_start,
     post_breakpoint_C_inflow = advance_df.loc[breakpoint_C:, 'nVehContrib'].sum()
     t_phase_end = advance_df.index[-1]
 
-    phase_end_queue_veh = max(0, max_queue_veh
-                                 - post_max_queue_outflow
-                                 + post_breakpoint_C_inflow)
-
+    phase_end_queue_veh = (max_queue_veh - post_max_queue_outflow
+                           + post_breakpoint_C_inflow)
 
     # speed of the queue discharge wave
     # v3 = max_queue_m - inter_detector_distance / (breakpoint_C - t_queue_max)
