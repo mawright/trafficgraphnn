ad101f6493a2739f3c1e40bff1c5ff2828f153ea
Matthew A. Wright <mwright@berkeley.edu>
2019-03-22 14:56:50 -0700

    new visualization code

--- trafficgraphnn/visualization.py
+++ trafficgraphnn/visualization.py
@@ -1,19 +1,142 @@
-#!/usr/bin/env python3
 """
 Code for visualizations.
 """
+import os
+import re
+import multiprocessing
+from itertools import repeat
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+
 import keras.backend as K
-from keras.losses import mean_absolute_percentage_error, mean_absolute_error
+from keras.losses import mean_absolute_error, mean_absolute_percentage_error
+from trafficgraphnn.load_data import pad_value_for_feature
+from trafficgraphnn.preprocessing.io import \
+    green_phase_start_ends_from_lane_light_df
+
+
+def get_figure_dir(results_filename):
+    results_dir = os.path.dirname(results_filename)
+    return os.path.join(results_dir, 'figures')
+
+
+def plot_results_for_file(filename):
+    fig_dir = get_figure_dir(filename)
+    with pd.HDFStore(filename, 'r') as store:
+        prefixes = prefixes_in_store(store)
+
+        for prefix in prefixes:
+            lanes = store[prefix + '/X'].index.get_level_values('lane').unique()
+
+    with multiprocessing.Pool() as pool:
+        pool.starmap(_plot_for_lane,
+                        zip(repeat(filename), repeat(fig_dir), repeat(prefix),
+                            lanes))
+
+
+def prefixes_in_store(store):
+    keys = store.keys()
+    prefixes = [re.search('(?<=/).+(?=/X|/Y)', key).group() for key in keys]
+    return sorted(list(set(prefixes)))
+
+
+def _plot_for_lane(store_filename, output_dir, prefix, lane_id):
+    with pd.HDFStore(store_filename, 'r') as store:
+        liu_series = (store[prefix + '/X'].loc[:, 'liu_estimated_veh']
+                                          .xs(lane_id, level='lane'))
+        max_jam_series = (store[prefix + '/Y']
+                          .loc[:, 'e2_0/maxJamLengthInVehicles']
+                          .xs(lane_id, level='lane'))
+        predicted_max_jamseries = (store[prefix + '/Yhat']
+                                  .loc[:, 'e2_0/maxJamLengthInVehicles']
+                                  .xs(lane_id, level='lane'))
+
+        green_series = (store[prefix + '/X'].loc[:, 'green']
+                                            .xs(lane_id, level='lane'))
+        vehseen_series = (store[prefix + '/Y'].loc[:, 'e2_0/nVehSeen']
+                                              .xs(lane_id, level='lane'))
+        predicted_vehseen_series = (store[prefix + '/Yhat']
+                                    .loc[:, 'e2_0/nVehSeen']
+                                    .xs(lane_id, level='lane'))
+
+    fig, ax = lane_queue_liu_vs_nn(liu_series, max_jam_series,
+                                    predicted_max_jamseries)
+    fig.savefig(os.path.join(output_dir, 'queue_estimate',
+                             '{}.eps'.format(lane_id)),
+                bbox_inches='tight')
+
+    fig, ax = lane_nvehseen_plot(green_series, vehseen_series,
+                                 predicted_vehseen_series)
+    fig.savefig(os.path.join(output_dir, 'vehseen_estimate',
+                             '{}.eps'.format(lane_id)),
+                bbox_inches='tight')
+
+
+def lane_queue_liu_vs_nn(liu_series, max_jam_series, predicted_series):
+    non_pad_timesteps = (max_jam_series
+                         != pad_value_for_feature['maxJamLengthInVehicles'])
+
+    fig, ax = plt.subplots()
+    ax.plot(max_jam_series.loc[non_pad_timesteps]
+                          .reset_index(drop=True, name='cycle'),
+                          'k', label='True cycle queue')
+
+    ax.plot(liu_series.loc[non_pad_timesteps]
+                      .reset_index(drop=True, name='cycle'),
+            'b--', label='Physics-based estimate')
+    ax.plot(predicted_series.loc[non_pad_timesteps]
+                            .reset_index(drop=True, name='cycle'),
+            'r:', label='Neural-net estimate')
+    ax.legend()
+    ax.set_xlabel('Cycle')
+    ax.set_ylabel('Queue (vehicles)')
+    return fig, ax
+
+
+def lane_nvehseen_plot(true_series, predicted_series, green_series=None):
+    fig, ax = plt.subplots()
+
+    # color green and red lights
+    if green_series is not None:
+        ax.axvspan(green_series.index[0], green_series.index[-1],
+                alpha=0.5, color='red') # red background
+        green_phases = green_phase_start_ends_from_lane_light_df(green_series)
+        for phase in green_phases:
+            ax.axvspan(phase[0], phase[1], alpha=0.5, color='green')
+
+    ax.plot(true_series, label='True')
+    ax.plot(predicted_series, label='Prediction')
+    ax.legend()
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Vehicles on lane')
+    return fig, ax
+
 
 def resample_predictions(predictions):
     sess = tf.InteractiveSession()
     #rearrange samples that we get the dimension [lanesxtimexprediction]
     num_samples = predictions.shape[0]
     timesteps_per_sample = predictions.shape[1]
-    num_lanes = predictions.shape[2]
+    num_lanes = predictions.shdef lane_nvehseen_plot(true_series, predicted_series, green_series=None):
+    fig, ax = plt.subplots()
+
+    # color green and red lights
+    if green_series is not None:
+        ax.axvspan(green_series.index[0], green_series.index[-1],
+                alpha=0.5, color='red') # red background
+        green_phases = green_phase_start_ends_from_lane_light_df(green_series)
+        for phase in green_phases:
+            ax.axvspan(phase[0], phase[1], alpha=0.5, color='green')
+
+    ax.plot(true_series, label='True')
+    ax.plot(predicted_series, label='Prediction')
+    ax.legend()
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Vehicles on lane')
+    return fig, axape[2]
 
     predictions_resampled = np.zeros((num_lanes, num_samples*timesteps_per_sample))
     for lane in range(num_lanes):
